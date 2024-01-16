213859def7633330a43cfd3a07dfc80faee43a7b
Iyanuoluwa fanoro <iycodes@outlook.com>
2024-01-16 09:08:44 +0100

    bd

---
+++ network.js
@@ -0,0 +1,84 @@
+//use this to store contract Addresses and ABI's
+
+db = {
+
+}
+
+chainMap = {
+    1:{
+        name: "ETH MAIN",
+        symbol: "ETH",
+    } ,
+    3: {
+         name: "Ropsten Testnet",
+         symbol: "ETH",
+    },
+    4: {
+         name: "Rinkeby Testnet",
+         symbol: "ETH",
+        },
+    5: {
+         name: "Goerli Testnet",
+         symbol: "ETH",
+        },
+    42: {
+         name: "Kovan Testnet",
+         symbol: "ETH",
+        },
+    137: {
+         name: "Polygon Mainnet",
+         symbol: "MATIC",
+        },
+    80001: {
+         name: "Mumbai Testnet",
+         symbol: "MATIC",
+        },
+    43114: {
+         name: "Avalanche Mainnet",
+         symbol: "AVAX",
+        },
+    43113: {
+         name: "Fuji Testnet",
+         symbol: "AVAX",
+        },
+    1088: {
+         name: "Metis Andromeda Mainnet",
+         symbol: "METIS",
+        },
+    588: {
+         name: "Metis Stardust Testnet",
+         symbol: "METIS",
+        },
+    1313161554: {
+         name: "Aurora Mainnet",
+         symbol: "AOA",
+        },
+    1313161555: {
+         name: "Aurora Testnet",
+         symbol: "AOA",
+        },
+    56: {
+         name: "BSC",
+         symbol: "BNB",
+        },
+    97: {
+         name: "BSC TEST",
+         symbol: "BNB",
+        },
+    250: {
+         name: "Fantom Opera Mainnet",
+         symbol: "FTM",
+        },
+    4002: {
+         name: "Fantom Testnet",
+         symbol: "FTM",
+    },
+    81: {
+         name: "Shibuya Mainnet",
+         symbol: "SBY",
+     },
+	 25: {
+         name: "CRONOS Mainnet",
+         symbol: "CRO",
+     },
+    }
